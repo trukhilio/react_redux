5832caaac309e235746a06ad15de758c2bdc908e
trukhilio <trukhantech@gmail.com>
2017-06-22 15:52:14 +0300

    add react-redux lib, connect and all elements from index.html goes to app.js

--- react_redux_monster/src/index.js
+++ react_redux_monster/src/index.js
@@ -1,15 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-// import './index.css';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-function playList (state = [], action){
+const initialState = [
+    'Smells like teen spirit',
+    'Am I Evil'
+];
+
+function playList (state = initialState, action){
     if (action.type === 'ADD_TRACK'){
         return [
             ...state,
@@ -21,21 +23,44 @@ function playList (state = [], action){
 
 const store = createStore(playList);
 
-const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-const trackInput = document.querySelectorAll('.trackInput')[0];
-const list = document.querySelectorAll('.list')[0];
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
-store.subscribe(() => {
-    list.innerHTML = '';
-    trackInput.value = '';
-    store.getState().forEach(track => {
-        const li = document.createElement('li');
-        li.textContent = track;
-        list.appendChild(li);
-    });
-});
+// registerServiceWorker();
 
-addTrackBtn.addEventListener('click', () => {
-    const trackName = trackInput.value;
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName});
-});
+// import { createStore } from 'redux';
+//
+// function playList (state = [], action){
+//     if (action.type === 'ADD_TRACK'){
+//         return [
+//             ...state,
+//             action.payload
+//         ];
+//     }
+//     return state;
+// }
+//
+// const store = createStore(playList);
+//
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+//
+// store.subscribe(() => {
+//     list.innerHTML = '';
+//     trackInput.value = '';
+//     store.getState().forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li);
+//     });
+// });
+//
+// addTrackBtn.addEventListener('click', () => {
+//     const trackName = trackInput.value;
+//     store.dispatch({ type: 'ADD_TRACK', payload: trackName});
+// });
